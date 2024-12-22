d71630ccb5fb59e0f23af29954bec7c5164fcaf4
오병주 <obj98@sjinc.co.kr>
2024-12-23 00:56:51 +0900

    ldJson 추가

--- src/app/board/[slug]/page.tsx
+++ src/app/board/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import {Metadata} from "next";
 import {redirect} from "next/navigation";
+import Script from 'next/script'
 
 import {EBannerType, EBlank} from '@/types/enums/common-enum';
 import {IPostData} from "@/types/interfaces/post-interface";
@@ -7,6 +8,7 @@ import {getParamSlugs, getPostBySlug} from "@/utils/postUtil";
 import {IMetadata} from "@/types/interfaces/metadata-interface";
 import {META} from "@/contants/metadata";
 import {getMetadata} from "@/seo/metadata/getMetadata";
+import {getLdJsonArticle} from "@/seo/ldJson/getLdJsonArticle";
 
 import Blank from '@/components/blank/Blank';
 import PageContainer from '@/components/containers/PageContainer';
@@ -40,9 +42,8 @@ export async function generateMetadata({params}: Props): Promise<Metadata> {
 export async function generateStaticParams() {
     return getParamSlugs();
 }
-
-// 할일 ldjson 추가하기 --> Script(next 전용) 태그 이용해야함
-// 스크롤 제어 생각해보기     https://velog.io/@seesaw/Next.js%EC%97%90%EC%84%9C-%EC%8A%A4%ED%81%AC%EB%A1%A4-%EC%9C%84%EC%B9%98-%EC%9C%A0%EC%A7%80%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95-Scroll-Restoration
+// 스크롤 제어 생각해보기
+// https://velog.io/@seesaw/Next.js%EC%97%90%EC%84%9C-%EC%8A%A4%ED%81%AC%EB%A1%A4-%EC%9C%84%EC%B9%98-%EC%9C%A0%EC%A7%80%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95-Scroll-Restoration
 
 const Post = (props: Props) => {
     const post: IPostData | undefined = getPostBySlug(props.params.slug);
@@ -51,28 +52,46 @@ const Post = (props: Props) => {
         redirect('/');
     }
 
+    const jsonLd = getLdJsonArticle({
+        id: `${META.baseUrl}/board/${post.slug}`,
+        headline: post.title,
+        image: [post.thumbnail],
+        datePublished: post.datePublished,
+        dateModified: post.dateModified,
+        author: { name: post.author, url: '' },
+        keywords: post.keywords,
+    });
+
     return (
-        <PageContainer>
-            <Blank type={EBlank.Header}/>
-            {
-                !post ?
-                    <Banner type={EBannerType.Home} title={""}/>
-                    :
-                    <>
-                        <Banner type={EBannerType.Read} title={post.title} author={post.author}
-                                dateModified={post.dateModified}/>
-                        <BodyContainer>
-                            <ContentsContainer>
-                                <Summary description={post.description}/>
-                                <EditorSection post={post} readOnly={true}/>
-                                <WriterInfo author={post.author}/>
-                                <TagList tags={post.keywords}/>
-                                {/*<ActivityBox slug={post.slug}/>*/}
-                            </ContentsContainer>
-                        </BodyContainer>
-                    </>
-            }
-        </PageContainer>
+        <>
+            <Script
+                id="json-ld"
+                type="application/ld+json"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+            />
+            <PageContainer>
+                <Blank type={EBlank.Header}/>
+                {
+                    !post ?
+                        <Banner type={EBannerType.Home} title={""}/>
+                        :
+                        <>
+                            <Banner type={EBannerType.Read} title={post.title} author={post.author}
+                                    dateModified={post.dateModified}/>
+                            <BodyContainer>
+                                <ContentsContainer>
+                                    <Summary description={post.description}/>
+                                    <EditorSection post={post} readOnly={true}/>
+                                    <WriterInfo author={post.author}/>
+                                    <TagList tags={post.keywords}/>
+                                    {/*<ActivityBox slug={post.slug}/>*/}
+                                </ContentsContainer>
+                            </BodyContainer>
+                        </>
+                }
+            </PageContainer>
+        </>
     );
 };
 
